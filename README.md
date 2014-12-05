c00db801f33b5c91f33abfa85bc9f68d5e71dd17
Anton Katsarov <anton@webface.bg>
2014-12-05 17:17:26 +0200

    * Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+ActiveAuth Pronto!
+===============
+
+**ActiveAuth Pronto!** - two-factor authentication for Pronto!
+
+
+## Instalation
+
+* Create WebSkin named `hPronto-`
+* Upload all the files in the `Skin` folder
+* Create a symbolic link of the original Pronto! login file:
+```
+ln -s /opt/CommuniGate/WebSkins/hPronto-/login.wssp /var/CommuniGate/WebSkins/hPronto-/activeauth.wssi
+```
+* Restart CommuniGateProt
+
+## Configuration
+
+### 0. Before you start, generate an application key:
+
+Your application key (or akey) is a string that you should generate and keep secret from ActiveAuth. It should be 40 characters long and stored alongside your integration key, secret key, integration account and integration server in configuration. This should be done once.
+
+You can generate a random string in Perl with:
+
+```
+perl -e 'print join "", map { sprintf "%08X", rand(0xffffffff) } 1 .. 5'
+```
+### 1. Edit the configuration file
+
+* Open `activeauth_config.wcgi` whit your favorite editor and fill in integration `server`,`ikey` for Integration ID, `iaccount` for integration account, `skey` for server key and `akey` for the application key from the previous step.
+* Reupload `activeauth_config.wcgi` to your skin files.
---
+++ Skin/pronto_activeauth.js
@@ -0,0 +1,68 @@
+/*
+
+  Copyright 2014 Anton Katsarov <anton@webface.bg>
+
+  Distributed under the MIT License.
+
+  See accompanying file LICENSE or copy at
+  http://opensource.org/licenses/MIT
+
+*/
+
+var template = '<div class="pronto-pref">\
+<cg-prefs-checkbox pref-name="Twofa" pref-label="Two-factor authentication enabled" selected="Twofa" pref-changed="TwofaChanged" on-change="onChange(pref)" pref-default="false"></cg-prefs-checkbox>\
+</div>';
+
+function watchPrefs() {
+    var mailElement = document.querySelector(".pronto-navigation-item_type_mail");
+    if (!mailElement) {
+	setTimeout(watchPrefs, 1000);
+	return;
+    } else {
+	// Pronto! has loaded
+	var injector = angular.element("body").injector();
+	injector.invoke(function (pubsubService) {
+	    function onModuleLoaded(info) {
+		var moduleName = info.data[0];
+		// contacts-core
+		if (moduleName == "prefs") {
+		    injector.invoke(function ($preferencesService) {
+			require(["angularAMD"], function (angularAMD) {
+			    var directiveMaker = angularAMD.getCachedProvider("$compileProvider");
+			    directiveMaker.directive('cgTwofaPrefs', function() {
+				return {
+				    restrict: "E",
+				    template: template,
+				    controller: function($scope, prefsService, $timeout) {
+					$scope.onChange = function (prefName) {
+					    // alert(prefsService.getStringPref("Twofa", false, false));
+					    prefsService.setPrefString("Twofa", ! $scope.Twofa);
+					    // Always use timeout to have possibility other pref panes to save their values cumulative way
+					    $timeout(function () {
+						prefsService.prefsUpdate();
+						$scope.wasSaved($scope);
+					    }, 10, false);
+
+					};
+				    }
+				};
+			    });
+			    $preferencesService.addPrefsPages([{
+				menuName: "Two-factor authentication",
+				iconClass: "pronto-folder_type_password",
+				header: "Two-factor authentication",
+				moduleId: "Preferences",
+				content: "<cg-twofa-prefs></cg-twofa-prefs>",
+				id: "Twofa",
+				priority: 0
+			    }]);
+			});
+ 		    });
+		    pubsubService.unsubscribe("moduleIsLoadedEvent", onModuleLoaded);
+		}
+	    }
+	    pubsubService.subscribe("moduleIsLoadedEvent", onModuleLoaded);
+	});
+    };
+};
+watchPrefs();
